cfa3b1f3a94bb10ab0d0165453703807c8ff7d09
Edward Zhou <esz7923@nyu.edu>
2024-10-17 04:43:06 -0400

    falling letters and working text input

--- src/components/PhysicsWorld.jsx
+++ src/components/PhysicsWorld.jsx
@@ -1,25 +1,18 @@
-import {
-  AccumulativeShadows,
-  Box,
-  ContactShadows,
-  GradientTexture,
-  MeshReflectorMaterial,
-  PerspectiveCamera,
-  RandomizedLight
-} from '@react-three/drei';
+import { PerspectiveCamera } from '@react-three/drei';
 import { Physics } from '@react-three/rapier';
-import { memo, useCallback, useRef, useState } from 'react';
+import { memo, useRef, useState } from 'react';
 import { gsap } from 'gsap';
+import { useFrame, useThree } from '@react-three/fiber';
 
 import Terrain from './Terrain';
-import { COLORS, sunPosition, WORLD_DEPTH, WORLD_WIDTH } from '../lib/constants';
-import { useFrame } from '@react-three/fiber';
+import { sunPosition, WORLD_DEPTH, WORLD_WIDTH } from '../lib/constants';
 import TextInput from './TextInput';
 
 export default memo(function PhysicsWorld({}) {
   const [isMoving, setIsMoving] = useState(false);
   const [textSelected, setTextSelected] = useState(false);
   const [textOffset, setTextOffset] = useState({ x: -110, y: 60, z: 360 });
+
   const cameraRef = useRef();
   const cameraVel = useRef(0);
 
@@ -50,19 +43,21 @@ export default memo(function PhysicsWorld({}) {
   });
 
   return (
-    <Physics debug colliders={false}>
-      <PerspectiveCamera
-        fov={50}
-        near={0.1}
-        far={5000}
-        position={[-110, 60, 360]}
-        onUpdate={(self) => self.lookAt(100, 0, -150)}
-        makeDefault
-        ref={cameraRef}
-      />
-      <TextInput origin={[-110, 60, 360]} offset={textOffset} setTextSelected={setTextSelected} />
-      {/* <TestSpheres /> */}
-      <Terrain width={WORLD_WIDTH} depth={WORLD_DEPTH} sunPosition={sunPosition} moveCamera={moveCamera} />
-    </Physics>
+    <>
+      <Physics debug colliders={false} gravity={[0, -100, 0]}>
+        <PerspectiveCamera
+          fov={50}
+          near={0.1}
+          far={5000}
+          position={[-110, 60, 360]}
+          onUpdate={(self) => self.lookAt(100, 0, -150)}
+          makeDefault
+          ref={cameraRef}
+        />
+        <TextInput origin={[-110, 60, 360]} offset={textOffset} setTextSelected={setTextSelected} />
+        {/* <TestSpheres /> */}
+        <Terrain width={WORLD_WIDTH} depth={WORLD_DEPTH} sunPosition={sunPosition} moveCamera={moveCamera} />
+      </Physics>
+    </>
   );
 });
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,12 +1,22 @@
-import { Box, Html, MeshTransmissionMaterial } from '@react-three/drei';
+import { Box, GradientTexture, Html, MeshTransmissionMaterial, Text3D } from '@react-three/drei';
 import { gsap } from 'gsap';
 import { memo, useEffect, useRef, useState } from 'react';
 import { COLORS } from '../lib/constants';
+import { RigidBody } from '@react-three/rapier';
+
+const translate = { x: 5, y: 0.5, z: -40 };
+const scale = 1.5;
 
 export default memo(function TextInput({ origin, offset, setTextSelected }) {
   const [selected, setSelected] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const [fallen, setFallen] = useState([]);
+
+  const selectedRef = useRef(false);
+  const charactersRef = useRef([]);
+
   const groupRef = useRef();
-  const inputRef = useRef();
+  const cursorRef = useRef();
 
   useEffect(() => {
     const { x, y, z } = offset;
@@ -15,58 +25,135 @@ export default memo(function TextInput({ origin, offset, setTextSelected }) {
       y,
       z,
       duration: 5,
-      delay: 3,
+      delay: 2,
       ease: 'power2.inOut'
     });
   }, [offset]);
 
+  useEffect(() => {
+    gsap.fromTo(
+      cursorRef.current.scale,
+      { x: 0.25, y: 1.5, z: 0.6 },
+      { x: 0, y: 0, z: 0, duration: 0.5, ease: 'expo.inOut', yoyo: true, yoyoEase: 'expo.inOut', repeat: -1 }
+    );
+
+    const handleTyping = (ev) => {
+      if (!selectedRef.current) return;
+      switch (ev.key) {
+        case 'Enter':
+          const newFallen = charactersRef.current.map((char, i) => ({
+            sourcePos: [
+              i * scale * 0.6 + translate.x - scale * 4.5 + groupRef.current.position.x,
+              translate.y - 0.3 * scale + groupRef.current.position.y,
+              translate.z + groupRef.current.position.z
+            ],
+            char
+          }));
+          setFallen((state) => [...state, ...newFallen]);
+        case 'Escape':
+          setCharacters([]);
+          charactersRef.current = [];
+          setSelected(false);
+          selectedRef.current = false;
+          setTextSelected(false);
+          break;
+        case 'Backspace':
+          setCharacters((state) => state.slice(0, -1));
+          charactersRef.current.pop();
+          break;
+        default:
+          if (ev.key.length !== 1 || charactersRef.current.length >= 14) break;
+          setCharacters((state) => [...state, ev.key]);
+          charactersRef.current.push(ev.key);
+      }
+    };
+    window.addEventListener('keydown', handleTyping);
+    return () => window.removeEventListener('keydown', handleTyping);
+  }, []);
+
   const emissive = selected ? COLORS.redOrange : '#666666';
   return (
     <>
-      <mesh position={origin} ref={groupRef}>
+      <mesh
+        position={origin}
+        ref={groupRef}
+        onClick={(ev) => {
+          ev.stopPropagation();
+          if (!selected) {
+            setSelected(true);
+            setTextSelected(true);
+            selectedRef.current = true;
+          }
+        }}
+        onPointerMissed={(ev) => {
+          if (selected) ev.stopPropagation();
+          setSelected(false);
+          selectedRef.current = false;
+          setTextSelected(false);
+        }}
+      >
+        <Box position={[translate.x, translate.y, translate.z]} scale={[10.25 * scale, 2.25 * scale, 0.61 * scale]} visible={false} />
+
+        <Text3D
+          font="courier.json"
+          position={[translate.x - 5 * scale, translate.y + (scale * 3) / 2, translate.z]}
+          size={scale / 2}
+          bevelEnabled
+          bevelSize={scale / 20}
+          bevelSegments={2}
+          bevelThickness={scale / 10}
+        >
+          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
+          Enter Message:
+        </Text3D>
+        <Box position={[translate.x, translate.y - scale, translate.z]} scale={[10 * scale, 0.25 * scale, 0.55 * scale]}>
+          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
+        </Box>
+        <Box position={[translate.x, translate.y + scale, translate.z]} scale={[10 * scale, 0.25 * scale, 0.55 * scale]}>
+          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
+        </Box>
+        <Box position={[translate.x - 5 * scale, translate.y, translate.z]} scale={[0.25 * scale, 2.25 * scale, 0.6 * scale]}>
+          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
+        </Box>
+        <Box position={[translate.x + 5 * scale, translate.y, translate.z]} scale={[0.25 * scale, 2.25 * scale, 0.6 * scale]}>
+          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
+        </Box>
+
+        {characters.map((char, i) => (
+          <Text3D
+            font="courier.json"
+            position={[i * scale * 0.6 + translate.x - scale * 4.5, translate.y - 0.3 * scale, translate.z]}
+            size={0.7 * scale}
+            bevelEnabled
+            bevelSize={scale / 20}
+            bevelSegments={2}
+            bevelThickness={scale / 20}
+            key={i}
+          >
+            <meshPhysicalMaterial color={'white'} emissive={'#AAAAAA'} iridescence={0.8} />
+            {char}
+          </Text3D>
+        ))}
         <Box
-          position={[5, 0.5, -10]}
-          scale={[10.25, 2.25, 0.61]}
-          onClick={(ev) => {
-            ev.stopPropagation();
-            inputRef.current.focus();
-            if (!selected) {
-              setSelected(true);
-              setTextSelected(true);
-            }
-          }}
-          on
-          onPointerMissed={(ev) => {
-            if (selected) ev.stopPropagation();
-            inputRef.current.blur();
-            setSelected(false);
-            setTimeout(() => {
-              setTextSelected(false);
-            }, 1000);
-          }}
-          visible={false}
-        />
-        <Html style={{ opacity: 0 }} prepend>
-          <input ref={inputRef} onChange={(val) => console.log(val)} onBlur={() => console.log('blurred')}></input>
-        </Html>
-
-        <Box position={[5, -0.5, -10]} scale={[10, 0.25, 0.55]}>
-          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
-        </Box>
-        <Box position={[5, 1.5, -10]} scale={[10, 0.25, 0.55]}>
-          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
-        </Box>
-        <Box position={[0, 0.5, -10]} scale={[0.25, 2.25, 0.6]}>
-          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
-        </Box>
-        <Box position={[10, 0.5, -10]} scale={[0.25, 2.25, 0.6]}>
-          <meshPhysicalMaterial color={'white'} emissive={emissive} iridescence={0.8} />
-        </Box>
-
-        <Box position={[0.5, 0.5, -10]} scale={[0.25, 1.5, 0.6]}>
+          position={[characters.length * scale * 0.6 + translate.x - scale * 4.2, translate.y, translate.z]}
+          scale={[0.25 * scale, 1.5 * scale, 0.6 * scale]}
+          visible={selected}
+          ref={cursorRef}
+        >
           <MeshTransmissionMaterial roughness={0.4} anisotropicBlur={0.3} transmissionSampler thickness={4} />
         </Box>
       </mesh>
+
+      {fallen.map(({ sourcePos, char }, i) => (
+        <RigidBody colliders="cuboid" key={i} position={sourcePos}>
+          <Text3D font="courier.json" size={0.7 * scale} bevelEnabled bevelSize={scale / 20} bevelSegments={2} bevelThickness={scale / 10}>
+            <meshPhysicalMaterial iridescence={0.8}>
+              <GradientTexture stops={[0, 1]} colors={[COLORS.lightOrange, COLORS.redOrange]} />
+            </meshPhysicalMaterial>
+            {char}
+          </Text3D>
+        </RigidBody>
+      ))}
     </>
   );
 });
